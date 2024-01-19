bdcc5842cfd7c6ae470e453755b487979ab5be77
Kota Bercey <108497613+koota1@users.noreply.github.com>
2024-01-19 15:21:59 -0800

    set up react app and imported aws amplify

---
+++ krtq-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "KRTQ": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3krtqstorage0daf01f1": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
